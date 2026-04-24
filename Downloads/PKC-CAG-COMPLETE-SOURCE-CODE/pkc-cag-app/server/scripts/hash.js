db579c44cf0be0048df631e9177dff8394bdce3c
Abhishek <abhigowda20201@gmail.com>
2026-04-24 21:44:47 +0530

    Delete Downloads/PKC-CAG-COMPLETE-SOURCE-CODE/pkc-cag-app directory

--- Downloads/PKC-CAG-COMPLETE-SOURCE-CODE/pkc-cag-app/server/scripts/hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-(async () => {
-  const plain = "Smitha@1022";
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(plain, salt);
-  console.log("HASH:\n", hash);
-})();
